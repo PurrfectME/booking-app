83b4f4755c0c5a1767a3b2c676ac6b25ace773c2
dmitry <dmitry@example.com>
2023-08-24 01:59:32 +0300

    refactor need to imrove

--- lib/screens/tables_scheme/tables_scheme_screen.dart
+++ lib/screens/tables_scheme/tables_scheme_screen.dart
@@ -10,54 +10,88 @@ class TablesSchemeScreen extends StatefulWidget {
 }
 
 class _TablesSchemeScreenState extends State<TablesSchemeScreen> {
-  List<Widget> movableItems = [];
+  List<Color> boxColors = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.yellow,
+  ];
+
+  List<Positioned> droppedRectangles = [];
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            movableItems.add(MoveableStackItem(xPosition: 0, yPosition: 0));
-          });
-        },
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Drag and Drop Rectangles')),
+      body: Center(
+        child: Column(
+          children: [
+            _buildDraggableBoxes(),
+            SizedBox(height: 20),
+            Expanded(child: _buildDropZone()),
+          ],
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: Stack(
-              children: movableItems,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              GestureDetector(
-                onPanUpdate: (tapInfo) {
-                  movableItems.add(MoveableStackItem(xPosition: ,));
-                  setState(() {
-                    xPosition += tapInfo.delta.dx;
-                    yPosition += tapInfo.delta.dy;
-                  });
-                },
-                child: Container(
-                  width: 150,
-                  height: 150,
-                  color: Colors.amberAccent,
+    );
+  }
+
+  Widget _buildDropZone() => Container(
+        color: Colors.grey,
+        child: DragTarget<int>(
+          builder: (context, candidateData, rejectedData) {
+            return Stack(
+              children: droppedRectangles,
+            );
+          },
+          onWillAccept: (data) {
+            return true;
+          },
+          onAcceptWithDetails: (data) {
+            setState(() {
+              droppedRectangles.add(
+                Positioned(
+                  left: data.offset.dx,
+                  top: data.offset.dy,
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    color: boxColors[2],
+                  ),
                 ),
-              ),
-              Container(
-                width: 150,
-                height: 150,
-                color: Colors.amberAccent,
-              ),
-              Container(
-                width: 150,
-                height: 150,
-                color: Colors.amberAccent,
-              ),
-            ],
-          )
-        ],
-      ));
+              );
+            });
+          },
+        ),
+      );
+
+  Widget _buildDraggableBoxes() {
+    return Container(
+      height: 100,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: boxColors
+            .asMap()
+            .map((index, color) => MapEntry(index, _buildDraggableBox(index)))
+            .values
+            .toList(),
+      ),
+    );
+  }
+
+  Widget _buildDraggableBox(int colorIndex) {
+    final color = boxColors[colorIndex];
+    return Draggable<int>(
+      data: colorIndex,
+      feedback: Container(
+        width: 50,
+        height: 50,
+        color: color.withOpacity(0.7),
+      ),
+      child: Container(
+        width: 50,
+        height: 50,
+        color: color,
+      ),
+    );
+  }
 }
